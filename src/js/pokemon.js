490cb677e1f18dc263ec31911028ba5ab45840a0
Kaden Beck <kbeck1@byupathway.edu>
2025-08-04 15:09:44 -0600

    Formatting Pokemon Page

--- src/js/pokemon.js
+++ src/js/pokemon.js
@@ -6,7 +6,7 @@ import PokemonDetails from "./PokemonDetails.mjs";
 loadHeaderFooter();
 
 // Get PokemonID from URL parameters, create a new PokemonDetails Object to store the data for that pokemon
-const pokemonID = getParam("id");
+const pokemonID = getParam("PokemonID");
 const pokemon = new PokemonDetails(pokemonID);
 
 // Initialize and render content for page
